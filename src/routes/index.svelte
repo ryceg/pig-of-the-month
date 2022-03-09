bb365fb51655f0d2a09584471462400db52996a5
Rhys <36913739+ryceg@users.noreply.github.com>
2022-03-09 20:27:46 +1100

    centre

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Little_Peegz</h1>
-<p>Documenting the wholesome activities of three guinea pigs</p>
+<div class="w-10/12 md:w-7/12 lg:6/12 mx-auto relative py-20">
+	<h1 class="text-3xl text-center font-bold text-blue-500">Little_Peegz</h1>
+	<p>Documenting the wholesome activities of three guinea pigs</p>
+</div>
