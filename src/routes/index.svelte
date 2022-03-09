69a8ff8df399d1adb19fed909c4dccc421365e26
Rhys <36913739+ryceg@users.noreply.github.com>
2022-03-10 10:37:39 +1100

    fix card

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,18 +7,14 @@
 		Documenting the wholesome activities of three guinea pigs
 	</p>
 	<div
-		class="hover:-translate-y-1 hover:scale-110 border-slate-500 md:text-4xl text-violet-600 hover:text-violet-500 bg-gradient-to-br from-green-200 to-blue-200 motion-reduce:transition-none motion-reduce:hover:transform-none block p-24 text-2xl font-bold transition duration-300 ease-in-out delay-150 bg-blue-500 border-4 rounded-lg"
+		class="hover:-translate-y-1 hover:scale-110 border-slate-100 md:text-4xl text-violet-600 hover:text-violet-500 motion-reduce:transition-none motion-reduce:hover:transform-none block m-4 text-2xl font-bold transition duration-300 ease-in-out delay-150 bg-white border-4 rounded-lg shadow-lg"
 	>
+		<img
+			class="bg-origin-border bg-clip-border bg-cover"
+			src="static/img/family-photo.jpg"
+			alt="A family portrait of the three guinea pigs"
+		/>
 		<a href="./pigs">Meet the peegz</a>
-	</div>
-
-	<div class="flex flex-col w-full mb-20 text-center">
-		<h2 class="sm:text-3xl title-font mb-4 text-2xl font-medium text-gray-900">
-			Three Little Pigs Went Out To Play
-		</h2>
-		<p class="lg:w-2/3 mx-auto text-base leading-relaxed">
-			And nothing bad happened. They had a lovely time, and then had veggies for dinner.
-		</p>
 	</div>
 
 	<a href="/.potm">View previous Pig of the Month winners</a>
